d6ca970d7fd28ab6f42cba5200fa6b7391304f84
Azizbek Abdurashidov <20221500443@newuzbekistanuniversity.uz>
2023-11-08 16:49:03 +0500

    task1

--- src/task_1.h
+++ src/task_1.h
@@ -3,29 +3,60 @@
  * Date:
  * Name:
  */
+
+
 class Node {
 public:
     int data;
     Node* next;
 
-    //Constructor
-    Node(int val){
+ 
+    Node(int val) {
         this->data = val;
-        this->next = NULL;
+        this->next = nullptr;
     }
 };
 
-class Stack{
+class Stack {
     Node* head;
+
 public:
-    //Constructor
-    Stack(){
-        this->head = NULL;
+  
+    Stack() {
+        this->head = nullptr;
     }
-    void push(int val){
+
+    void push(int val) {
         Node* tmp = new Node(val);
-        tmp->data = val;
         tmp->next = this->head;
         this->head = tmp;
     }
+
+    
+    int pop() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty. Cannot pop." << std::endl;
+            return -1; 
+        }
+        int poppedValue = head->data;
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+        return poppedValue;
+    }
+
+    
+    int top() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty. Cannot get top element." << std::endl;
+            return -1; 
+        }
+        return head->data;
+    }
+
+  
+    bool isEmpty() {
+        return head == nullptr;
+    }
 };
+
